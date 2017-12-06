ed809b8088f04397934344b8ff8cf73f616c7375
Khalid Al-Ghunaim <kfg2000@live.com>
2017-12-06 20:00:28 +0300

    authentication stuff

---
+++ simsim/urls.py
@@ -0,0 +1,10 @@
+from . import views
+from django.urls import path
+
+app_name="simsim"
+
+urlpatterns = [
+	path('signup/',views.usersignup, name='signup'),
+    path('login/',views.userlogin, name='login'),
+    path('logout/', views.userlogout, name='logout')
+]
--- simsim/views.py
+++ simsim/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserSignup, UserLogin
+from django.contrib.auth import authenticate, login, logout
 
-# Create your views here.
+def usersignup(request):
+	context = {}
+	form = UserSignup()
+	context['form'] = form
+
+	if request.method=='POST':
+		form = UserSignup(request.POST)
+		if form.is_valid():
+			user = form.save()
+			username = user.username
+			password = user.password
+
+			user.set_password(password)
+			user.save()
+
+			auth_user = authenticate(username=username,password=password)
+			login(request, user)
+			return redirect('/')
+		return redirect('simsim:signup')
+	return render(request,'signup.html',context)
+
+def userlogin(request):
+	context = {}
+	form = UserLogin()
+	context['form'] = form
+
+	if request.method=='POST':
+		form = UserLogin(request.POST)
+		if form.is_valid():
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password']
+
+			user = authenticate(username=username, password=password)
+
+			if user is not None:
+				login(request, user)
+				return redirect('/')
+			return redirect("simsim:login")
+		return redirect("simsim:login")
+	return render(request,'login.html',context)
+
+def userlogout(request):
+	logout(request)
+	return redirect('/')
+
+
